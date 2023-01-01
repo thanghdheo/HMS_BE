55201c9c63e299cb935a29d0a2ff1b65a29be795
luvyhoa <luvyhoa@outlook.com>
2023-01-01 23:31:16 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.enableCors();
+  app.enableCors();
   app.setGlobalPrefix('api');
   //app.use(morgan('tiny', { stream: logStream }));
+  var corsOptions = {
+    origin: [
+      `http://localhost:3000`,
+      `https://hotel-management-1bgq.vercel.app`,
+    ],
+  };
+  app.use(cors(corsOptions));
   await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
