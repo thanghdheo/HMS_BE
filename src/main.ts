f1c05561bfd5d34f6e5d2af8f63750bb20a56021
Lu Hoa <>
2023-01-01 12:25:56 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as morgan from 'morgan';
-import * as fs from 'fs';
-const logStream = fs.createWriteStream('api.log', {
-  flags: 'a', // append
-});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  app.use(morgan('tiny', { stream: logStream }));
+  //app.use(morgan('tiny', { stream: logStream }));
   await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
