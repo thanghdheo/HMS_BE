25e0075f7491dab0ff8b217d326f64be7087c7b4
luvyhoa <luvyhoa@outlook.com>
2023-01-01 23:24:12 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //app.enableCors();
   app.setGlobalPrefix('api');
   //app.use(morgan('tiny', { stream: logStream }));
   await app.listen(parseInt(process.env.PORT));
