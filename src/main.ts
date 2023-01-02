02e9f6f1772d4ed038873a3c4fe11b506f4ec8a5
luvyhoa <luvyhoa@outlook.com>
2023-01-02 21:31:03 +0700

    update main

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  //app.enableCors();
   app.setGlobalPrefix('api');
   //app.use(morgan('tiny', { stream: logStream }));
   var corsOptions = {
