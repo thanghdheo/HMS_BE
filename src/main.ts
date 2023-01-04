0de1c3c38cd7d4dd76ee8c89a0027396d972d3ad
THẮNG <thang.ho@fdssoft.com>
2023-01-05 00:29:50 +0700

    update: cors ne

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule)
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+  //app.use(cors(options))
+  app.enableCors(options);
   app.setGlobalPrefix('api');
   await app.listen(parseInt(process.env.PORT));
 }
