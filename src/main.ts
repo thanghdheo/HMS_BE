4474ec1d17dd72dc1235d7586acfd0addd160dd7
THẮNG <thang.ho@fdssoft.com>
2023-01-04 23:01:55 +0700

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 const cors = require('cors');
 
 async function bootstrap() {
-  var corsOptions = {
-    origin: [
-      `http://localhost:3000`,
-      `https://hotel-management-1bgq.vercel.app`,
-    ],
-  };
+  // var corsOptions = {
+  //   origin: [
+  //     `http://localhost:3000`,
+  //     `https://hotel-management-1bgq.vercel.app`,
+  //   ],
+  // };
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.use(cors(corsOptions));
+  // app.use(cors(corsOptions));
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
   app.setGlobalPrefix('api');
   await app.listen(parseInt(process.env.PORT));
 }
