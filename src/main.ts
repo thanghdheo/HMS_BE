18885b652c84f964a862b01bc0cee428df14932d
THẮNG <thang.ho@fdssoft.com>
2023-01-04 23:09:32 +0700

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    allowedHeaders: [
+      'origin',
+      'x-requested-with',
+      'content-type',
+      'accept',
+      'authorization',
+    ],
+    credentials: true,
+    origin: ['http://localhost:3001', 'http://localhost:3000'],
+  };
+  app.enableCors(corsOptions);
   app.setGlobalPrefix('api');
   await app.listen(parseInt(process.env.PORT));
 }
